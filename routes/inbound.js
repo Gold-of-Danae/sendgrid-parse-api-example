5e929b0ad108e8ed0c170b4e0143c4f7b448bac0
Gold-of-Danae <daniellamlatham@gmail.com>
2020-02-09 16:45:51 -0600

    Hacking the parse

--- routes/inbound.js
+++ routes/inbound.js
@@ -11,16 +11,16 @@ var inbound = {
 
     var Email     = sendgrid.Email;
     var email     = new Email({
-      to:       to,
-      from:     "hi@sendgrid-parse-api-example.com",
-      subject:  "[sendgrid-parse-api-example] Inbound Payload",
-      text:     "A payload was just delivered via SendGrid's Inbound Parse API. It should be attached."
+      to:       "daniellamlatham@gmail.com",
+      from:     "inbound@daniellalatham.com",
+      subject:  "[Incoming Email] From:" + to,
+      text:     JSON.stringify(payload)
     });
 
-    email.addFile({
-      filename: 'payload.txt',
-      content: new Buffer(JSON.stringify(payload))
-    });
+    //email.addFile({
+    //  filename: 'payload.txt',
+    //  content: new Buffer(JSON.stringify(payload))
+    //});
 
     sendgrid.send(email, function(err, json) {
       if (err) { 
